c9783990068690f306111a630f9edce6a56e10af
Usama Subhani <reachusama@protonmail.com>
2020-11-05 00:28:10 +0500

    Get bookmarks from faunadb

--- functions/bookmarks/bookmarks.js
+++ functions/bookmarks/bookmarks.js
@@ -1,37 +1,45 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
+const faunadb = require('faunadb'),
+  q = faunadb.query;
+
+var client = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET });
 
 const typeDefs = gql`
   type Query {
-    hello: String
-    allAuthors: [Author!]
-    author(id: Int!): Author
-    authorByName(name: String!): Author
+    bookmarks: [Bookmark!]
   }
-  type Author {
-    id: ID!
-    name: String!
-    married: Boolean!
+  type Bookmark {
+    url: String!
+    pageTitle: String!
+    description: String!
   }
 `
 
-const authors = [
-  { id: 1, name: 'Terry Pratchett', married: false },
-  { id: 2, name: 'Stephen King', married: true },
-  { id: 3, name: 'JK Rowling', married: false },
+const bookmarks = [
+  {
+    "url": "https://medium.com/incerto/religion-violence-tolerance-progress-nothing-to-do-with-theology-a31f351c729e?s=09",
+    "pageTitle": "Religion, Violence, Tolerance & Progress: Nothing to do with Theology",
+    "description": "Religions create highly differentiated belief clusters and mentalities that have little to do with their theologies."
+  },
+  {
+    "url": "https://medium.com/incerto/religion-violence-tolerance-progress-nothing-to-do-with-theology-a31f351c729e?s=09",
+    "pageTitle": "Title",
+    "description": "Religions create highly differentiated belief clusters and mentalities that have little to do with their theologies."
+  }
 ]
 
 const resolvers = {
   Query: {
-    hello: () => {
-      return 'Hello, world!'
-    },
-    allAuthors: () => {
-      return authors
-    },
-    author: () => {},
-    authorByName: (root, args) => {
-      console.log('hihhihi', args.name)
-      return authors.find((author) => author.name === args.name) || 'NOTFOUND'
+    bookmarks: async (parent, args, { user }) => {
+      const results = await client.query(
+        q.Paginate(q.Match(q.Index("all_bookmarks")))
+      );
+      return results.data.map(([url, pageTitle, description]) => ({
+        url,
+        pageTitle,
+        description
+      }));
+      // return bookmarks
     },
   },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from "react"
+import { useQuery,useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+
+const BookMarksQuery = gql`{
+  bookmarks{
+    url
+    pageTitle
+    description
+  }
+}`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const {loading,error,data} = useQuery(BookMarksQuery)
+  console.log(data)
+  
+    if (data) {
+      return <p>{JSON.stringify(data)}</p>
+    }
+    else {
+      return <p>Not Found</p>
+    }
 }
