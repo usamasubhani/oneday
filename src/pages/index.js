1d37054d4fd77b1be74378b2da7d060e74a8b8e0
Usama Subhani <reachusama@protonmail.com>
2020-11-12 00:41:49 +0500

    Implement UI using bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const AddBookMarkMutation = gql`
 export default function Home() {
   const { loading, error, data } = useQuery(BookMarksQuery)
   const [addBookmark] = useMutation(AddBookMarkMutation)
-
+  console.log(data);
   let url, title, desc;
 
   const addBookmarkSubmit = () => {
@@ -36,15 +36,64 @@ export default function Home() {
     console.log('Desc', desc.value)
   }
 
+  if (loading) {
+    return <p>Loading..</p>
+  } else if (error) {
+    return <p>{error}</p>
+  }
+  else {
+    return (
+      <>
+
+        <div class="jumbotron">
+          <h1 class="display-3">One Day!</h1>
+          <p class="lead">I will read/watch everything I saved here.</p>
+          <hr class="my-4" />
+          <p>This app is developed using Gatsby, Netlify and FaunaDB</p>
+          <p>View the source code on <a href="https://github.com/usamasubhani/oneday">Github</a>. Connect with me on <a href="https://twitter.com/basedusama">Twitter</a>.</p>
+        </div>
+        <div className="container">
+          <div className="container">
+            <form>
+              <label>URL</label>
+              <input type="text" className="form-control" id="url" ref={node => url = node} />
+              <label>Title</label>
+              <input type="text" className="form-control" id="title" ref={node => title = node} />
+              <label>Description</label>
+              <input type="text" className="form-control" id="description" ref={node => desc = node} />
+              <button className="btn btn-primary mt-2" onClick={addBookmarkSubmit}>Add</button>
+            </form>
+          </div>
+
+          {data.bookmarks.map((bookmark) => {
+            return (<>
+              {/* <p>{bookmark.url}</p>
+            <p>{bookmark.pageTitle}</p>
+            <p>{bookmark.description}</p> */}
+
+              <div className="col-sm-12">
+                <div className="card border-secondary min-vh-500 m-3 p-3">
+                  <h5 className="card-title">{bookmark.pageTitle}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{bookmark.description}</h6>
+                  <a href={bookmark.url} className="card-text">{bookmark.url}</a>
+                </div>
+              </div>
+            </>)
+          })}
+
+        </div>
+      </>
+    )
+  }
   return (
     <div>
-      <p>{JSON.stringify(data)}</p>
+      {/* <p>{JSON.stringify(data)}</p>
       <div>
         <input type="text" placeholder="URL" ref={node => url = node} />
         <input type="text" placeholder="Page Title" ref={node => title = node} />
         <input type="text" placeholder="Description" ref={node => desc = node} />
         <button onClick={addBookmarkSubmit}>Add BookMark</button>
-      </div>
+      </div> */}
     </div>
   )
   // if (data) {
